14192d8c598f1f8bdff3fc0527b9d8979cac334d
Douglas Greenshields <douglas@usemarkup.com>
2015-11-17 11:58:35 +0000

    add a high level is-archived filter

---
+++ tests/Filter/IsArchivedFilterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Markup\Contentful\Tests\Filter;
+
+use Markup\Contentful\Filter\IsArchivedFilter;
+use Mockery as m;
+
+class IsArchivedFilterTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $this->value = true;
+        $this->filter = new IsArchivedFilter(true);
+    }
+
+    protected function tearDown()
+    {
+        m::close();
+    }
+
+    public function testIsFilter()
+    {
+        $this->assertInstanceOf('Markup\Contentful\FilterInterface', $this->filter);
+    }
+
+    public function testGetKey()
+    {
+        $this->assertEquals('sys.archivedVersion[exists]', $this->filter->getKey());
+    }
+
+    /**
+     * @dataProvider values
+     */
+    public function testValueIsBoolean($original, $boolean)
+    {
+        $filter = new IsArchivedFilter($original);
+        $this->assertSame($boolean, $filter->getValue());
+    }
+
+    public function values()
+    {
+        return [
+            ['true', true],
+            [0, false],
+        ];
+    }
+}
