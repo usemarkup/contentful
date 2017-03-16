b2a4c595ad042325b1be115a72ed6b53f41e452b
Douglas Greenshields <dgreenshields@gmail.com>
2017-03-16 10:45:51 +0000

    add support for guzzle 6 (using psr7), dropping guzzle 4 (#15)

--- src/Exception/ResourceUnavailableException.php
+++ src/Exception/ResourceUnavailableException.php
@@ -3,28 +3,29 @@
 namespace Markup\Contentful\Exception;
 
 use GuzzleHttp\Message\ResponseInterface;
+use GuzzleHttp\Psr7\Response;
 
 class ResourceUnavailableException extends \RuntimeException implements ExceptionInterface
 {
     /**
-     * @var ResponseInterface
+     * @var ResponseInterface|Response
      */
     private $response;
 
     /**
-     * @param ResponseInterface $response
-     * @param string            $message
-     * @param int               $code
-     * @param \Exception        $previous
+     * @param ResponseInterface|Response $response
+     * @param string                     $message
+     * @param int                        $code
+     * @param \Exception                 $previous
      */
-    public function __construct(ResponseInterface $response = null, $message = '', $code = 0, \Exception $previous = null)
+    public function __construct($response = null, $message = '', $code = 0, \Exception $previous = null)
     {
         $this->response = $response;
         parent::__construct($message, $code, $previous);
     }
 
     /**
-     * @return \GuzzleHttp\Message\ResponseInterface|null
+     * @return \GuzzleHttp\Message\ResponseInterface|Response|null
      */
     public function getResponse()
     {
