41d8fa8478f852f0e30631b6d28c2a8b6447d98c
Douglas Greenshields <dgreenshields@gmail.com>
2017-04-09 19:48:26 +0100

    use phpunit 5

--- tests/Decorator/NullAssetDecoratorTest.php
+++ tests/Decorator/NullAssetDecoratorTest.php
@@ -2,6 +2,8 @@
 
 namespace Markup\Contentful\Tests\Decorator;
 
+use Markup\Contentful\AssetInterface;
+use Markup\Contentful\Decorator\AssetDecoratorInterface;
 use Markup\Contentful\Decorator\NullAssetDecorator;
 
 class NullAssetDecoratorTest extends \PHPUnit_Framework_TestCase
@@ -13,12 +15,12 @@ class NullAssetDecoratorTest extends \PHPUnit_Framework_TestCase
 
     public function testIsDecorator()
     {
-        $this->assertInstanceOf('Markup\Contentful\Decorator\AssetDecoratorInterface', $this->decorator);
+        $this->assertInstanceOf(AssetDecoratorInterface::class, $this->decorator);
     }
 
     public function testDecorationReturnsSameAsset()
     {
-        $asset = $this->getMock('Markup\Contentful\AssetInterface');
+        $asset = $this->createMock(AssetInterface::class);
         $this->assertSame($asset, $this->decorator->decorate($asset));
     }
 }
